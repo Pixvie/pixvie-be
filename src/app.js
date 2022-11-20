80426936c82bcc565c33c9c9a7cfeb72e2de940d
Altay Simsek <altaysimsek16@gmail.com>
2022-11-20 18:15:18 +0300

    feat: setting socket.io on express server

--- src/app.js
+++ src/app.js
@@ -1,18 +1,37 @@
 require('dotenv').config();
+const { Server } = require("socket.io");
 const express = require('express');
+const http = require('http');
+
 
 //Routes files
 const auth = require('./routes/auth');
 
 require('./connection.js');
 const app = express();
-const port = 3000;
+const server = http.createServer(app);
+const io = new Server(server,{
+  cors: {
+    origin:"*",
+    methods:["GET", "POST", "OPTIONS"]
+  }
+});
+const port = process.env.PORT || 3000;
 
 app.use('/api/auth', auth);
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  socket.on('DRAW_PIXEL', (payload) => {
+    console.log(socket.id, payload)
+    socket.broadcast.emit("DRAWED_PIXEL", payload);
+  })
+
+});
+
+server.listen(port, () => {
   console.log(`🌊 [Server] - Listening on ${port}`);
 });
